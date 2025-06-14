d75c182c350ffb0488f9dee8ffbcd324403906e6
Jozinm <jozinm@gmail.com>
2025-06-14 22:01:09 +0000

    add model

--- default.sh
+++ default.sh
@@ -17,6 +17,7 @@ PIP_PACKAGES=(
 CHECKPOINT_MODELS=(
     "https://civitai.com/api/download/models/798204?type=Model&format=SafeTensor&size=full&fp=fp16"
     "https://civitai.com/api/download/models/501240?type=Model&format=SafeTensor&size=pruned&fp=fp16"
+    "https://civitai.com/api/download/models/192843?type=Model&format=SafeTensor"
 )
 
 UNET_MODELS=(
